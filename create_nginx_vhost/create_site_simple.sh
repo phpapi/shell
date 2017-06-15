7851f6d45ef95600b0790c8e778823b0b36e56d9
root <root@localhost.localdomain>
2017-06-15 17:12:49 +0800

    json output

--- create_nginx_vhost/create_site_simple.sh
+++ create_nginx_vhost/create_site_simple.sh
@@ -3,21 +3,37 @@ NGINX_SITES_ENABLED='/usr/local/nginx/conf/vhosts'
 WEB_DIR='/home/vagrant/sync/example'
 SED=`which sed`
 CURRENT_DIR=`dirname $0`
+status='1'
+date=`sudo date`
 
 if [ -z $1 ]; then
-	echo "No domain name given"
-	exit 1
+    status="0"
+	output="No domain name given"
+	echo -e "{\"status\":\""$status"\", \"msg\":\""$output"\",\"data\":\"""\", \"uptime\":\""$date"\"}"
+    exit;
 fi
-DOMAIN=$1
+
+prefix=$(date +%s%N)
+DOMAIN=$prefix.$1
 
 # check the domain is roughly valid!
 PATTERN="^([[:alnum:]]([[:alnum:]\-]{0,61}[[:alnum:]])?\.)+[[:alpha:]]{2,6}$"
 if [[ "$DOMAIN" =~ $PATTERN ]]; then
 	DOMAIN=`echo $DOMAIN | tr '[A-Z]' '[a-z]'`
-	echo "Creating hosting for:" $DOMAIN
+	output="Creating hosting for:" $DOMAIN
 else
-	echo "invalid domain name"
-	exit 1 
+    status="0"
+	output="invalid domain name"
+    echo -e "{\"status\":\""$status"\", \"msg\":\""$output"\",\"data\":\"""\", \"uptime\":\""$date"\"}"
+    exit;
+fi
+
+### check if domain already exists
+if [ -e $NGINX_CONFIG$DOMAIN ]; then
+    status="0"
+    output="This domain already exists.\nPlease Try Another one"
+    echo -e "{\"status\":\""$status"\", \"msg\":\""$output"\",\"data\":\"""\", \"uptime\":\""$date"\"}"
+    exit;
 fi
 
 #Replace dots with underscores
@@ -45,4 +61,26 @@ sudo cp $CURRENT_DIR/index.html.template $WEB_DIR/$SITE_DIR/index.html
 sudo $SED -i "s/SITE/$DOMAIN/g" $WEB_DIR/$SITE_DIR/index.html
 sudo chown vagrant:vagrant $WEB_DIR/$SITE_DIR/index.html
 
-echo "Site Created for $DOMAIN"
+### create mysql database
+#echo -e $"create mysql database core--$DOMAIN start \n"
+#sh /home/vagrant/sync/shell/create_nginx_vhost/mysql.sh "core"$prefix
+
+MYSQL_BIN="/usr/local/mysql/bin/mysql"
+MYSQL_ROOT="stars"
+MYSQL_PASS="123456"
+MYSQL_HOST="192.168.200.29"
+DB="core"$prefix
+#echo "DATABASES IS :"$DB":"
+if [ $MYSQL_PASS ]
+then
+  #echo "logging into as $MYSQL_ROOT"
+  sudo $MYSQL_BIN -u "$MYSQL_ROOT" -p"$MYSQL_PASS" -h"$MYSQL_HOST" -e "create database $DB"
+  #$MYSQL_BIN -u "$MYSQL_ROOT" -p"$MYSQL_PASS" -h"$MYSQL_HOST" -e "SHOW DATABASES"
+  sudo $MYSQL_BIN -u "$MYSQL_ROOT" -p"$MYSQL_PASS" -h"$MYSQL_HOST" $DB < /home/vagrant/sync/shell/create_nginx_vhost/core.sql
+else
+  $MYSQL_BIN -u "$MYSQL_ROOT" -e "SHOW DATABASES"
+fi
+
+output='Site Created for '$DOMAIN
+echo -e "{\"status\":\""$status"\", \"msg\":\""$output"\",\"data\":\""$DOMAIN"\", \"uptime\":\""$date"\"}"
+exit;
