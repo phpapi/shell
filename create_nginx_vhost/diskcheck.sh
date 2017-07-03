45518e2b908da2d914896d2b46c0aa73bb6b539e
Carey <phpapi@163.com>
2017-07-03 11:12:17 +0800

    diskcheck

---
+++ create_nginx_vhost/diskcheck.sh
@@ -0,0 +1,30 @@
+#£¡/bin/bash
+              
+FSMAX="80"
+remote_user='root'
+remote_ip=(192.168.33.10,192.168.33.10)
+ip_num='2'
+echo "$ip_num"
+
+while [ "$ip_num" -le "$(expr ${#remote_ip[@]} -l)"]
+do
+        read_num='1'
+        ssh "$remote_user"@"${remote_ip[$ip_num]}"  df -h > /tmp/diskcheck_tmp
+        grep '^/dev/*'  /tmp/diskcheck_tmp | awk '{print $5}'|sed 's/\%//g'  > /tmp/diskcheck_num_tmp
+    
+
+            while [ "$read_num" -le $(wc -l < /tmp/diskcheck_num_tmp) ]
+            do
+                    size=$(sed -n "$read_num" 'p'  /tmp/diskcheck_num_tmp)
+                            if [ "size" -gt "$FSMAX" ]
+                            then
+                                    $(grep '^/dev/*'  /tmp/diskcheck_tmp |sed -n $read_num'p'  > /tmp/disk_check_mail)
+                                    $(echo ${remote_ip[$ip_num]}) >> /tmp/disk_check_mail
+                                    $(mail  -s "diskcheck_alert"  admin  <  /tmp/disk_check_mail)
+                            fi
+                    read_num=$(expr  $read_num + 1)
+            done
+        ip_num=$(expr  $ip_num + 1)
+done
+#              ===================Ð´Èëcrontab=====================
+#                0/10 * * * *   /home/diskcheck.sh   2&>1
