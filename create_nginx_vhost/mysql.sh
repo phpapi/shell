3e3fce196f51fdb2a09ac6719768c58a2c80788e
Carey <phpapi@163.com>
2017-07-10 11:13:32 +0800

    mysql

--- create_nginx_vhost/mysql.sh
+++ create_nginx_vhost/mysql.sh
@@ -2,10 +2,9 @@ MYSQL_ROOT="stars"
 MYSQL_PASS="123456"
 MYSQL_HOST="192.168.200.29"
 DB=$1
-echo "DATABASES IS :"$DB":"
+
 if [ $MYSQL_PASS ]
 then
-  echo "logging into as $MYSQL_ROOT"
   sudo mysql -u "$MYSQL_ROOT" -p"$MYSQL_PASS" -h"$MYSQL_HOST" -e "create database $DB"
   sudo mysql -u "$MYSQL_ROOT" -p"$MYSQL_PASS" -h"$MYSQL_HOST" -e "SHOW DATABASES"
   sudo mysql -u "$MYSQL_ROOT" -p"$MYSQL_PASS" -h"$MYSQL_HOST" $DB < core.sql
