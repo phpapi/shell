b79f8aca30b7e98a7527f5b71419fea676c8bec0
Carey <phpapi@163.com>
2017-07-12 15:55:20 +0800

    mysql

--- create_nginx_vhost/mysql.sh
+++ create_nginx_vhost/mysql.sh
@@ -1,3 +1,4 @@
+MYSQL_BIN="/usr/local/mysql/bin/mysql"
 MYSQL_ROOT="stars"
 MYSQL_PASS="123456"
 MYSQL_HOST="192.168.200.29"
@@ -8,6 +9,11 @@ then
   sudo mysql -u "$MYSQL_ROOT" -p"$MYSQL_PASS" -h"$MYSQL_HOST" -e "create database $DB"
   sudo mysql -u "$MYSQL_ROOT" -p"$MYSQL_PASS" -h"$MYSQL_HOST" -e "SHOW DATABASES"
   sudo mysql -u "$MYSQL_ROOT" -p"$MYSQL_PASS" -h"$MYSQL_HOST" $DB < core.sql
+  sudo $MYSQL_BIN -u "$MYSQL_ROOT" -p"$MYSQL_PASS" -h"$MYSQL_HOST" -e "use master"
+  SQL="INSERT INTO address_config (pc_address,h5_address,admin_address,created,updated)VALUES('$DOMAIN','m.$DOMAIN','imc.$DOMAIN',$TIME,$TIME)"
+  sudo $MYSQL_BIN -u "$MYSQL_ROOT" -p"$MYSQL_PASS" -h"$MYSQL_HOST" $DB_NAME << EOF
+    $SQL
+EOF
 else
   sudo mysql -u "$MYSQL_ROOT" -e "SHOW DATABASES"
 fi
